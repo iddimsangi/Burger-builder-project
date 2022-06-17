c1f7d55417348911a8c855ff39d0f791c728e147
iddimsangi <adammsangi@gmail.com>
2022-06-17 15:12:21 +0300

    Add components and setup project structure

---
+++ src/Components/Burger/BurgerIngridients/BurgerIngridients.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import classes from "./BurgerIngridients.css";
+import PropTypes from 'prop-types'
+
+export class BurgerIngridients extends Component {
+
+  render() {
+    let ingridients = null;
+    switch (props.type) {
+      case "bread-bottom":
+        ingridients = <div className={classes.BreadBottom}></div>;
+        break;
+      case "bread-top":
+        ingridients = (
+          <div className={classes.BreadTop}>
+            <div className={classes.Seeds1}></div>
+            <div className={classes.Seeds2}></div>
+          </div>
+        );
+        break;
+      case "meat":
+        ingridients = <div className={classes.Meat}></div>;
+        break;
+      case "cheese":
+        ingridients = <div className={classes.Cheese}></div>;
+        break;
+      case "salad":
+        ingridients = <div className={classes.Salad}></div>;
+        break;
+      case "bacon":
+        ingridients = <div className={classes.Bacon}></div>;
+        break;
+      default:
+        ingridients = null;
+        break;
+    }
+    return <div>BurgerIngridients</div>;
+  }
+}
+
+BurgerIngridients.PropTypes = {
+    type:PropTypes.string.isRequired
+}
+export default BurgerIngridients
+
